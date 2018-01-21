f3e6f54e2b02bf90884531adce4ddf78f8a82150
SUBHAN <subhan.ali@verscomtech.com>
2018-01-21 13:12:50 +0500

    # Comments added to Welcome page

--- HelloWorldForms/Pages/WelcomePage.cs
+++ HelloWorldForms/Pages/WelcomePage.cs
@@ -41,52 +41,93 @@ namespace HelloWorldForms
 
         }
 
-        //SignIn Click Handler
+        /**
+         * SignIn Click Handler
+         * After field validating set user status to login
+         * start Franchise location selection page
+         * set username and password entry to empty
+         * */
         async void Handle_Signin_Clicked(object sender, System.EventArgs e)
         {
             if(isValidData()){
+
+                //set login status
                 Settings.UserLoggedInStatus = true;
+
+                //start Frenchise selection page
                 await Navigation.PushAsync(new SelectFranchizsePage());
+
+                //set field data to empty
 				usernameEntry.Text = "";
 				passwordEntry.Text = "";
+
+                //remove current page from back stack to avoid navigation back to login page after successful login
                 Navigation.RemovePage(this);
             }
         }
 
-        //Skip Click Handler
+        /*
+         * Skip Click Handler
+         * Start frenchise selection page
+         * set fields data to empty
+         */
         async void Handle_Skip_Clicked(object sender, System.EventArgs e)
         {
+            //start fields navigation page
             await Navigation.PushAsync(new SelectFranchizsePage());
+
+            //se fields data to empty
 			usernameEntry.Text = "";
 			passwordEntry.Text = "";
         }
 
-        //Validate form data and show alert
+        /*
+         * Validate form data and show alert
+         * 
+         * Take data form fields and validate if it is not empty or null or whitespace and show warning alert 
+         * accordingly
+         * 
+         * Check if field data matches with the existing one in local stoareg
+         * 
+         */
         private bool isValidData()
         {
+            //Fetching signup model from storage
             SignupModel signUpModel = JsonConvert.DeserializeObject<SignupModel>(Settings.UserSignUpData);
+
+            //taking data form fields
             string username = usernameEntry.Text;
             string password = passwordEntry.Text;
 
+            //checking signup model's nullability
             if (signUpModel == null)
             {
+                //show alert if model is null
                 DisplayAlert("Error!", "User doesnot exist", "OK");
                 return false;
             }
             else
             {
+                //check if fields data is null or empty
                 if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
                 {
+                    //show alert dialog
                     DisplayAlert("Error!", "Please enter username and password", "OK");
                     return false;
                 }
+
+                //check if fields daata is null or comprising of empty space
                 else if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
                 {
+                    //show alert
                     DisplayAlert("Error!", "Please enter username and password", "OK");
                     return false;
                 }
+
+                //check if fields data matches with data from storage
                 else if (!username.Equals(signUpModel.username) && !password.Equals(signUpModel.password))
                 {
+                    //show alert
                     DisplayAlert("Error!", "Please enter correct username and password", "OK");
                     return false;
                 }
